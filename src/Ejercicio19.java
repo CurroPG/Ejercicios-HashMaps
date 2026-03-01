9ce97eda465964c556fca9e9a2a2d14c135b30e7
Curro Portillo <211curroportillo@gmail.com>
2026-03-01 12:35:12 +0100

    19

--- src/Ejercicio19.java
+++ src/Ejercicio19.java
@@ -14,38 +14,37 @@ public class Ejercicio19 {
         diccionario.put("grande", "big");
         ArrayList<String> keys = new ArrayList<>(diccionario.keySet());
 
-        do{
+        do {
             String palabra = System.console().readLine("Introduzca una palabra y le daré los sinónimos: ");
             String significado = diccionario.get(palabra);
             String sinonimos = "";
             String opcion = "";
             String nuevoSinonimo = "";
-            if(palabra.equals("salir"))
+            if (palabra.equals("salir"))
                 break;
-            if(!diccionario.containsKey(palabra)){
+            if (!diccionario.containsKey(palabra)) {
                 opcion = System.console().readLine("No conozco esa palabra ¿quiere añadirla al diccionario? (s/n): ");
-                if(opcion.equals("s")){
-                    significado = System.console().readLine("Introduzca la traducción de "+palabra+" en inglés: ");
+                if (opcion.equals("s")) {
+                    significado = System.console().readLine("Introduzca la traducción de " + palabra + " en inglés: ");
                     keys.add(palabra);
                     diccionario.put(palabra, significado);
                 }
-            }
-                else{
-                    for (String k : keys) {
-                if(diccionario.get(k).equals(significado) && !k.equals(palabra))
-                    sinonimos += k + ", ";
-            }
-            if(sinonimos.isEmpty()){
-                opcion = System.console().readLine("No conozco sinónimos de esa palabra ¿quiere añadir alguno? (s/n): ");
-                if(opcion.equals("s")){
-                    nuevoSinonimo += System.console().readLine("Introduzca un sinónimo de "+palabra+": ");
-                    keys.add(nuevoSinonimo);
-                    diccionario.put(nuevoSinonimo, diccionario.get(palabra));
+            } else {
+                for (String k : keys) {
+                    if (diccionario.get(k).equals(significado) && !k.equals(palabra))
+                        sinonimos += k + ", ";
                 }
+                if (sinonimos.isEmpty()) {
+                    opcion = System.console()
+                            .readLine("No conozco sinónimos de esa palabra ¿quiere añadir alguno? (s/n): ");
+                    if (opcion.equals("s")) {
+                        nuevoSinonimo += System.console().readLine("Introduzca un sinónimo de " + palabra + ": ");
+                        keys.add(nuevoSinonimo);
+                        diccionario.put(nuevoSinonimo, diccionario.get(palabra));
+                    }
+                } else
+                    System.out.println("Sinónimos de " + palabra + ": " + sinonimos);
             }
-            else
-                System.out.println("Sinónimos de "+palabra+": " + sinonimos);
-                }
-        }while(true);
+        } while (true);
     }
 }
